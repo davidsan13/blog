1fedc32c6c3b52868d6fc57fcabc3f70a08e0b4f
David <sandavid08@yahoo.com>
2024-03-06 19:04:54 -0800

    added new comp

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -19,14 +19,33 @@ function Blogs() {
   ]
 
   const allBlogs = blogs.map((blog) =>
-    <div className="blog-card">
-      <Link key={blog.id} to={{pathname: `/blog/${blog.id}`}}>{blog.title}</Link>
-    </div> 
+    // <div key={blog.id} className="blog-card">
+    //   <Link to={{pathname: `/blog/${blog.id}`}}>{blog.title}</Link>
+    // </div>
+    <article key={blog.id}>
+      <div className="article-wrapper">
+        <figure>
+          <img src="#" alt="" />
+        </figure>
+        <div className="article-body">
+          <h2>{blog.title}</h2>
+          <p>
+            {blog.content}
+          </p>
+          <Link className='read-more' to={{pathname: `/blog/${blog.id}`}}>
+            Read more <span className="sr-only">about this is some title</span>
+              <svg xmlns="http://www.w3.org/2000/svg" className="icon" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
+              </svg>  
+          </Link>
+        </div>
+      </div>
+    </article>
   )
  
   return (
     <>
-      <h1>Hello, Welcome to my blog page</h1>
+      <h1>Hello, Welcome to page</h1>
       
       <div className='blog-container'>
         {allBlogs}
