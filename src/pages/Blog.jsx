fbae269693340c3fa3ababbe75980b50cb06cd26
David <sandavid08@yahoo.com>
2024-06-12 16:48:20 -0700

    decode content

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,7 @@ import { useLocation, useParams } from 'react-router-dom'
 import GetData from '../components/Api'
 import Comment from '../components/Comment'
 import formatDate from '../assets/utilities/helper'
+import { decode } from 'html-entities'
 const Blog = () => {
   const [blog, setBlog] = useState([]);
   let {state} = useLocation()
@@ -13,12 +14,11 @@ const Blog = () => {
   },[state])
   
   const date = formatDate(blog.publishedAt)
-
   const blogView = 
     <div className="blog" key={blog._id}>
       <h1 className='blog-title'>{blog.title}</h1>
       <h2 className='blog-date'>{date}</h2>
-      <p className='blog-content'>{blog.content}</p>
+      <p className='blog-content'>{decode(blog.content)}</p>
       <Comment/>
     </div>
 
